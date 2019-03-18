a78785b7e347aba118219d9e68ed74cae78ef60d
cnagama2 <chikako_mkthrk@yahoo.co.jp>
2019-03-18 13:41:03 +0000

    リレーションのチェック

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -70,6 +70,8 @@ class TaskController extends Controller
      */
     public function showEditForm(Folder $folder, Task $task)
     {
+        $this->checkRelation($folder, $task);
+        
         return view('tasks/edit', [
             'task' => $task,
         ]);
@@ -84,6 +86,8 @@ class TaskController extends Controller
      */
     public function edit(Folder $folder, Task $task, EditTask $request)
     {
+        $this->checkRelation($folder, $task);
+        
         $task->title = $request->title;
         $task->status = $request->status;
         $task->due_date = $request->due_date;
@@ -93,4 +97,11 @@ class TaskController extends Controller
             'id' => $task->folder_id,
         ]);
     }
+    
+    private function checkRelation(Folder $folder, Task $task)
+    {
+        if ($folder->id !== $task->folder_id) {
+            abort(404);
+    }
+}  
 }
