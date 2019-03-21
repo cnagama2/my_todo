9ac8ec6c199a06ce0dfecaf7792c9a5b2aeed3e9
cnagama2 <chikako_mkthrk@yahoo.co.jp>
2019-03-21 13:24:26 +0000

    文字数制限AppServiceProvider修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
     {
         if (\App::environment('production')) {
         \URL::forceScheme('https');
+
+        Schema::defaultStringLength(191);
     }
     }
 }
